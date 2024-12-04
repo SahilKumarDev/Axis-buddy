b14db60c6090313c1d894bab0b9ebe5fac74fec6
sahilkumardev <sahilkumar710@outlook.com>
2024-12-04 07:01:25 +0530

    add shedule call funcinility

--- src/app/meeting/calendly.tsx
+++ src/app/meeting/calendly.tsx
@@ -9,7 +9,7 @@ const Calendly =  () => {
     <div className="w-full ">
       <InlineWidget
       styles={{ height: "1000px" }}
-      url="https://calendly.com/birdsoftware/meeting" />
+      url="https://calendly.com/sp7102004/30min" />
     </div> );
 }
  
--- src/app/meeting/page.tsx
+++ src/app/meeting/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-
 import Calendly from "./calendly";
-import ShowcaseNavbar from "@/components/showcase-navbar";
+// import ShowcaseNavbar from "@/components/showcase-navbar";
 import { PiCheckCircle } from "react-icons/pi";
 import { motion } from "framer-motion";
 
@@ -12,19 +11,12 @@ const checkItemVariants = {
 };
 
 const Meeting = () => {
-
-
-
   return (
-    <div className="
-    
-    flex flex-col  w-full  
-
-     overflow-clip inset-0 -z-10 
-  bg-[#fafafa] bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]
-    
-    ">
-      <ShowcaseNavbar />
+    <div
+      className="  
+    "
+    >
+      
       <div className="md:px-0 px-6 xl:w-4/5 2xl:w-[68%] justify-between md:mt-14 md:flex mx-auto  ">
         <div className="md:w-2/5">
           <h1 className="text-4xl font-semibold pt-10   ">Let&apos;s Meet</h1>
@@ -34,45 +26,45 @@ const Meeting = () => {
           </p>
 
           {[
-                {
-                  title: "Development + Design",
-                  description:
-                    "Turn your ideas into reality with our development and design services.",
-                },
+            {
+              title: "Development + Design",
+              description:
+                "Turn your ideas into reality with our development and design services.",
+            },
 
-                {
-                  title: "Free Consultation",
-                  description:
-                    "Get expert advice on how to improve your business and increase your online presence.",
-                },
-                {
-                  title: "Technical Support",
-                  description:
-                    "Get technical support for your website or application.",
-                },
-              ].map((item, index) => (
-                <motion.div
-                  key={index}
-                  variants={checkItemVariants}
-                  initial="hidden"
-                  animate="visible"
-                  transition={{ delay: index * 1.8 }}
-                  className="flex gap-x-4 py-4"
-                >
-                  <PiCheckCircle className=" rounded-md text-[#3d80d7] text-2xl flex-shrink-0" />
-                  <ul>
-                    <h3 className="text-lg font-bold text-gray-700">
-                      {item.title}
-                    </h3>
-                    <div className="text-gray-400">{item.description}</div>
-                  </ul>
-                </motion.div>
-              ))}
+            {
+              title: "Free Consultation",
+              description:
+                "Get expert advice on how to improve your business and increase your online presence.",
+            },
+            {
+              title: "Technical Support",
+              description:
+                "Get technical support for your website or application.",
+            },
+          ].map((item, index) => (
+            <motion.div
+              key={index}
+              variants={checkItemVariants}
+              initial="hidden"
+              animate="visible"
+              transition={{ delay: index * 1.8 }}
+              className="flex gap-x-4 py-4"
+            >
+              <PiCheckCircle className=" rounded-md text-[#3d80d7] text-2xl flex-shrink-0" />
+              <ul>
+                <h3 className="text-lg font-bold text-gray-700">
+                  {item.title}
+                </h3>
+                <div className="text-gray-400">{item.description}</div>
+              </ul>
+            </motion.div>
+          ))}
         </div>
 
-    <div className="md:w-1/2">
-      <Calendly />
-      </div>
+        <div className="md:w-1/2">
+          <Calendly />
+        </div>
       </div>
     </div>
   );
